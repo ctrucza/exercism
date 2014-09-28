73e60cf2cc46c0753f2e7f4ffb1fcc452268cede
Csaba Trucza <csaba.trucza@iquestgroup.com>
2014-09-28 13:07:53 +0300

    Leap year in C#

---
+++ csharp/leap/Year.cs
@@ -0,0 +1,16 @@
+﻿namespace Leap
+{
+    public class Year
+    {
+        public static bool IsLeap(int year)
+        {
+            if (year % 4 != 0)
+                return false;
+            if (year % 100 != 0)
+                return true;
+            if (year % 400 == 0)
+                return true;
+            return false;
+        }
+    }
+}
