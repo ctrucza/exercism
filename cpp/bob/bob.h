b7cdefac0400c7b49e1e15ccfe4efeb621673313
Csaba Trucza <csaba.trucza@iquestgroup.com>
2014-09-28 14:34:40 +0300

    Bob answers when you shout at him

--- cpp/bob/bob.h
+++ cpp/bob/bob.h
@@ -1,11 +1,24 @@
 #include <string>
+#include <ctype.h>
 using namespace std;
 
 class bob
 {
 public:
-    static string hey(string)
+    static string hey(string sentence)
     {
+        if (shouting(sentence))
+            return "Whoa, chill out!";
         return "Whatever.";
     };
+private:
+    static bool shouting(string sentence)
+    {
+        for (auto c : sentence)
+        {
+            if (!isupper(c))
+                return false;
+        }
+        return true;
+    }
 };
